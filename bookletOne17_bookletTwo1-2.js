60a87279ce801252fbd10fe04fbcc8b991383ab1
Neema Totonchi <ntotonchi1@se14l0121-08.ad.bcit.ca>
2022-10-27 15:19:25 -0700

    updates files

---
+++ bookletOne17_bookletTwo1-2.js
@@ -0,0 +1,40 @@
+//If/Else and Functions
+
+//1-1
+var number = 10;
+var sentence = "";
+
+if (number == 10) {
+    sentence = `You are right`;
+} else {
+    sentence = `You are wrong`;
+}
+
+console.log(sentence);
+
+//1-2
+var [hobby, city, message] = ["Gardening", "Vancouver", ""];
+
+if (hobby == "Gardening") {
+    message = `I live in ${city} and I like ${hobby}.`;
+} else {
+    message = `Not the right hobby.`;
+}
+
+console.log(message);
+
+//1-3
+
+//1-4
+
+
+function findGrade (name,mark) {
+    if (mark >= 85 && mark <= 100) {
+        console.log(`${name}, you have recieved A grade`)
+    } else if (mark >= 85 && mark <= 100) {
+        console.log(`${name}, you have recieved A grade`)
+    } else if (mark >=70 && mark <85) {
+console.log
+    }
+}
+
