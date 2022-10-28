69538de061d4c46b50c8096663cd9d652c5b8aca
Neema Totonchi <ntotonchi1@se14l0121-08.ad.bcit.ca>
2022-10-27 17:49:24 -0700

    updated files

--- bookletOne17_bookletTwo1-2.js
+++ bookletOne17_bookletTwo1-2.js
@@ -1,4 +1,92 @@
-//If/Else and Functions
+//Booklet 1
+
+//Objects + Console Log
+
+//17-1
+var student = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 22,
+    gpa:4.0
+}
+
+var sentence = `${student.firstName} ${student.lastName} is ${student.age} years old and has a ${student.gpa} GPA.`;
+
+console.log(sentence);
+
+//17-2
+var student = {
+    firstName: "Mark",
+    lastName: "Jacob",
+    age: 31,
+    id: "A001234"
+}
+
+var sentence = `${student.firstName} ${student.lastName} is ${student.age} years old. The employee ID is ${student.id}.`
+
+console.log(sentence);
+
+//17-3
+var p2 = new Object();
+
+p2.name = "Steve";
+p2.age = 12;
+
+console.dir(p2);
+
+//17-4
+var toyotaCorolla = new Object();
+
+toyotaCorolla.modeOne = "Sport";
+toyotaCorolla.modeTwo = "EV";
+toyotaCorolla.litre = 1.8;
+
+console.dir(toyotaCorolla);
+
+//17-5
+var microsoft = new Object();
+
+microsoft.laptop = "Surafe Laptop 4";
+microsoft.screenInches = 13.4;
+microsoft.memoryGb = 16;
+microsoft.batteryLifeHours = 19;
+microsoft.processor = "Quad Core 11th Intel Core i7-1185G7";
+
+console.dir(microsoft)
+console.log(microsoft.processor);
+
+//17-6
+var pet = new Object();
+
+pet.name = "Max";
+pet.age = 6;
+pet.type = "Guinea pig";
+pet.lifeSpan = 8;
+
+console.dir(pet);
+console.log(pet.age);
+
+//17-7
+var toyotaCorollaHybrid = new Object();
+
+toyotaCorollaHybrid.mode = ["Sport", "EV"];
+toyotaCorollaHybrid.litre = 1.8;
+toyotaCorollaHybrid.valve = 16;
+toyotaCorollaHybrid.cylinder = 4;
+
+console.dir(toyotaCorollaHybrid);
+
+var sentence=`The Toyota Corolla Hybrid is in ${toyotaCorollaHybrid.mode[0]} mode and can carry up to ${toyotaCorollaHybrid.litre} litres.`;
+
+console.log(sentence);
+
+//17-8
+
+
+
+//Booklet 2
+
+//1 - If/Else and Functions
 
 //1-1
 var number = 10;
@@ -24,17 +112,141 @@ if (hobby == "Gardening") {
 console.log(message);
 
 //1-3
+var firstName = `Steve`, age = 14, country = `Canada`, message = ``;
+
+var MINIMUM_AGE = 16;
+var LEGAL_AGE_COUNTRY = "Canada";
+
+if (age >= MINIMUM_AGE && country == LEGAL_AGE_COUNTRY) {
+    message = `You are able to get your license`;
+} else {
+    message = `You are a minor`;
+}
+
+console.log(message);
 
 //1-4
-
-
-function findGrade (name,mark) {
+function findGrade(name, mark) {
     if (mark >= 85 && mark <= 100) {
-        console.log(`${name}, you have recieved A grade`)
-    } else if (mark >= 85 && mark <= 100) {
-        console.log(`${name}, you have recieved A grade`)
-    } else if (mark >=70 && mark <85) {
-console.log
+        console.log(`${name}, you have recieved A grade.`);
+    } else if (mark >= 70 && mark < 85) {
+        console.log(`${name}, you have recieved B grade.`);
+    } else if (mark >= 50 && mark < 70) {
+        console.log(`${name}, you have recieved C grade.`);
+    } else if (mark >= 0 && mark < 50) {
+        console.log(`${name}, you have failed.`);
+    } else {
+        console.log(`Invalid mark of ${mark}.`);
     }
 }
+
+findGrade("Sarah", 91);//Sarah you recieved A grade.
+findGrade("Geroge", 85);//George you recieved A grade.
+findGrade("Sam", 75);//Sam you recieved B grade.
+findGrade("Emily", 53);//Emily you recieved C grade.
+findGrade("Tom", 20);//Tom you have failed.
+findGrade("Wilson", 120);//Invalid mark of 120.
+
+//1-5
+function evalNumbers(num1, num2, operator) {
+    if (operator == "add") {
+        console.log(`Sum of ${num1} and ${num2} is ${num1 + num2}.`);
+    } else if (operator == "subtract") {
+        console.log(`Difference of ${num1} and ${num2} is ${num1 - num2}.`);
+    } else if (operator == "multiply") {
+        console.log(`Product of ${num1} and ${num2} is ${num1 * num2}.`);
+    } else if (operator == "divide") {
+        console.log(`Division of ${num1} + ${num2} is ${num1 / num2}.`);
+    } else if (operator == "modulus") {
+        console.log(`Modulus of ${num1} and ${num2} is ${num1 % num2}.`);
+    } else {
+        console.log(`${operator} is an invalid operation.`);
+    }
+}
+
+evalNumbers(15, 10, "add");//Sum of 15 and 10 is 25.
+evalNumbers(20, 8, "subtract");//Difference of 20 and 8 is 12.
+evalNumbers(12, 4, "multiply");//Product of 12 and 4 is 48.
+evalNumbers(28, 7, "divide");//Division of 28 and 7 is 4.
+evalNumbers(22, 3, "modulus");//Modulus of 22 and 3 is 1.
+evalNumbers(31, 3, "square");//Square is an invalid operation.
+
+//1-6
+var num = 12;
+var message = "";
+
+if (num > 12) {
+    sentence = `Your number is too high.`;
+} else if (num == 12) {
+    sentence = `You guessed correct!`;
+} else {
+    sentence = `Your nmber is too low.`;
+}
+
+console.log(sentence); //You guessed correct!
+
+//1-7
+var num = Math.floor(Math.random() * (20 - 0 + 1) + 0);
+var message = ``;
+
+if (num > 12) {
+    message = `Your number is too high.`;
+} else if (num == 12) {
+    message = `You guessed correct!"`
+} else {
+    message = `Your number is too low.`
+}
+
+console.log(message);
+
+//1-8
+
+function isValidNumber(num) {
+    if (isNaN(num)) {
+        console.log(`${num} is not a number.`);
+    } else {
+        console.log(`${num} is a valid number.`)
+    }
+}
+
+isValidNumber(21); //21 is a valid number.
+isValidNumber(7); //7 is a valid number.
+isValidNumber("Abc"); //Abc is not a number.
+isValidNumber(12.5); //12.5 is a valid number.
+isValidNumber("21M");//21M is not a number.
+
+//2 - Error Handling
+
+//2-1
+var person = ``;
+var message = ``;
+
+try {
+    if (person.length == 0) {
+        throw `empty`;
+    } 
+    if (person.name == "Bob") {
+        console.log(`Message matched.`)
+    }
+} catch (err) {
+    console.log(`The person variable is ${err}`);
+}
+
+//2-2 
+var person = "Bob";
+var message = "";
+
+try {
+    if (person.length == 0) {
+        throw `empty`;
+    }
+    if (person == "Bob") {
+        console.log(`Message matched.`)
+    }
+}catch(err) {
+    console.log(`The person variable is ${err}`);
+} finally {
+    console.log(`Finished checking.`);
+}
+
 
