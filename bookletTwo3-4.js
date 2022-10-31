26f9ce0ace7fd203f390f5b7e8f3b4cbfba9f432
Neema Totonchi <ntotonchi1@my.bcit.ca>
2022-10-31 13:37:13 -0700

    updated files

--- bookletTwo3-4.js
+++ bookletTwo3-4.js
@@ -30,9 +30,128 @@ var text = ``;
 
 for (var i = 0; i < 7; i++) {
     if (i == 5) {
-        text += `Finished`;
+        text = `Finished`;
         break;
     }
 }
 
 console.log(`${text} at the number 5`);
+
+//3-1-6
+for (var i = 0;  i < 10; i++) {
+    if ((i % 2) ==  1) {
+        i +=3;
+    }
+    console.log(i);
+}
+console.log(`The number stopped at ${i}.`);
+
+//3-1-7
+var person = `bob`;
+
+for (var i = 0; i < 10; i++) {
+    if (i == 4 && person == `bob`) {
+        console.log(`The person ${person} has been selected`);
+        break;
+    }
+}
+console.log(`The number stopped at the value ${i}.`);
+
+//3-1-8
+var text = ``;
+
+for (var i = 0; i < 10; i++) {
+    if (i == 3) {
+        text += `Finished`;
+        break;
+    } else if (i == 5) {
+        i++
+    }
+}
+
+console.log(`${text} at the number ${i}`);
+
+//3-1-9 ***
+var text = ``;
+
+for (var i = 0; i < 10; i++) {
+    if (!((i % 2) == 0) && i == 1) {
+        console.log(`Not divisible by 2. The number is ${i}.`);
+    }
+}
+
+console.log(`Finished loop.`);
+
+//3-1-10 ***
+var text = ``;
+
+for (var i = 0; i < 10; i++) {
+    if (!((i % 3) == 0)) {
+        console.log(`Not divisible by 3. The number is ${i}.`);
+    }
+}
+
+console.log(`Finished Loop`);
+
+//3-1-11
+/**var text = ``;
+
+for (var i = 0; i < 10; i++) {
+
+}
+**/
+//3-1-12
+var drinks = [`pepsi`, `cola`, `orange juice`, `water`];
+var num = 0;
+
+for (var i = 0; i < 10; i++) {
+    if (drinks[1] == `cola`) {
+        console.log(`test`);
+    }
+}
+console.log(`didnt work`);
+
+//3-1-13 
+var text = ``;
+
+for (var i = 0; i < 10; i++) {
+    console.log(i);
+    if (i == 3) {
+        i++;
+    } else if (i == 5) {
+        text += `Finished`;
+        break;
+    }
+}
+
+console.log(`${text} at the number ${i}.`);
+
+//3-1-14
+var text = ``;
+
+for (var i = 0; i < 10; i++) {
+    console.log(i);
+    if (i == 3) {
+        i = Math.floor(Math.random() * (9-0 + 1)) + 0;
+    } else if (i == 5) {
+        text += `Finished`;
+        break;
+    }
+}
+
+console.log(`${text} at the value ${i}.`);
+
+//3-1-15
+var text = ``;
+
+for (var i = 0; i < 10; i++) {
+    if (i == 3) {
+        i = Math.floor(Math.random() * (9-0 + 1)) + 0;
+        console.log(`________`);
+    } else if (i == 5) {
+        i = 11;
+        console.log(`Finished at the number ${i}`);
+    }
+}
+
+
